74523cb669c2b3e321b633aaac7c327ce7939ddd
huu <hbw194@qq.com>
2017-12-08 08:35:19 +0800

    merge

--- search/help.js
+++ search/help.js
@@ -1,2 +1,6 @@
 // event loop
 // http://www.ruanyifeng.com/blog/2014/10/event-loop.html
+// list all installed package
+// npm list -g --depth=0
+<script src="js/require.js" defer async="true" ></script>
+// async属性表明这个文件需要异步加载，避免网页失去响应。IE不支持这个属性，只支持defer，所以把defer也写上。
